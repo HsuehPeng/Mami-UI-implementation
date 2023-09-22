bbb89919277807aa4adabdbbcfc40c05d36702a6
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-22 15:39:07 +0800

    Move UIImage extension to its own file

---
+++ mamilove_interview/UIImage+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+Extension.swift
+//  mamilove_interview
+//
+//  Created by Hsueh Peng Tseng on 2023/9/22.
+//
+
+import UIKit
+
+extension UIImage {
+	static func make(withColor color: UIColor) -> UIImage {
+		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+		UIGraphicsBeginImageContext(rect.size)
+		let context = UIGraphicsGetCurrentContext()!
+		context.setFillColor(color.cgColor)
+		context.fill(rect)
+		let img = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		return img!
+	}
+}
