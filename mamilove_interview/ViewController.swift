46081ce37d3ee36a4682734357565c29c6e67a35
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-22 10:31:14 +0800

    Create project

---
+++ mamilove_interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mamilove_interview
+//
+//  Created by Hsueh Peng Tseng on 2023/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
