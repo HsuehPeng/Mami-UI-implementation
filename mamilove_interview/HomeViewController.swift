d29a175dcb36598d98a33f9f56263d33d534ab8a
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-22 15:18:04 +0800

    HomeViewController - Add navigationBackgroundView

--- mamilove_interview/HomeViewController.swift
+++ mamilove_interview/HomeViewController.swift
@@ -8,13 +8,49 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+	// MARK: - Properties
+	
+	lazy var navigationBarBackgroundView: UIView = {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.backgroundColor = .systemPink
+		view.alpha = 0
+		return view
+	}()
+	
+	// MARK: - LifeCycle
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-		view.backgroundColor = .red
+		
+		configureNavBar()
+		configureUI()
 	}
+}
 
+// MARK: - UI Helpers
 
+extension HomeViewController {
+	private func configureNavBar() {
+		title = "媽咪愛 UI實作"
+		navigationController?.navigationBar.isTranslucent = true
+		navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .topAttached, barMetrics: .default)
+		navigationController?.navigationBar.shadowImage = UIImage()
+		navigationController?.navigationBar.tintColor = .white
+		navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Right", style: .plain, target: nil, action: nil)
+	}
+	
+	private func configureUI() {
+		view.addSubview(navigationBarBackgroundView)
+
+		NSLayoutConstraint.activate([
+			navigationBarBackgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+			navigationBarBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			navigationBarBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			navigationBarBackgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+		])
+	}
 }
 
