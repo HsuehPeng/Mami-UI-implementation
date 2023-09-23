36266df5d6c7b9ed8479107eab4ebdc62474bca4
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-23 18:14:45 +0800

    HomeViewControllerViewModel.loadCheckoutInfo - CheckoutInfoLoader calls load

---
+++ mamilove_interview/CheckoutInfo.swift
@@ -0,0 +1,12 @@
+//
+//  CheckoutInfo.swift
+//  mamilove_interview
+//
+//  Created by Hsueh Peng Tseng on 2023/9/23.
+//
+
+import Foundation
+
+struct CheckoutInfo {
+	
+}
--- mamilove_interviewTests/mamilove_interviewTests.swift
+++ mamilove_interviewTests/mamilove_interviewTests.swift
@@ -8,29 +8,49 @@
 import XCTest
 @testable import mamilove_interview
 
+protocol CheckoutInfoLoader {
+	typealias LoadResult = Result<CheckoutInfo, Error>
+	func load(completion: @escaping (LoadResult) -> Void)
+}
+
+class HomeViewControllerViewModel {
+	let checkoutInfoLoader: CheckoutInfoLoader
+	
+	init(checkoutInfoLoader: CheckoutInfoLoader) {
+		self.checkoutInfoLoader = checkoutInfoLoader
+	}
+	
+	func loadCheckoutInfo() {
+		checkoutInfoLoader.load { result in
+			
+		}
+	}
+}
+
 final class mamilove_interviewTests: XCTestCase {
+	
+	func test_loadCheckoutInfo_loaderDidCallLoad() {
+		let (sut, loader) = makeSut()
+		
+		sut.loadCheckoutInfo()
+		
+		XCTAssertTrue(loader.didCallLoad)
+	}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	// MARK: - Helpers
+	
+	private func makeSut() -> (HomeViewControllerViewModel, CheckoutInfoLoaderSpy) {
+		let checkoutInfoLoader = CheckoutInfoLoaderSpy()
+		let sut = HomeViewControllerViewModel(checkoutInfoLoader: checkoutInfoLoader)
+		return (sut, checkoutInfoLoader)
+	}
+							   
+	class CheckoutInfoLoaderSpy: CheckoutInfoLoader{
+		var didCallLoad = false
+		
+		func load(completion: @escaping (LoadResult) -> Void) {
+			didCallLoad = true
+		}
+	}
 
 }
